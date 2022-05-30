9f3dc09212a7817d63d03a452af62c7e8245b59e
Do Hyun Nam <dollis2002@gmail.com>
2022-05-30 07:51:38 -0500

    Fixed more styles

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -7,17 +7,22 @@ import { Link } from "react-router-dom";
 export default function Signup() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
   const [error, setError] = useState("");
 
   const { signup, currentUser, authError } = useAuth();
 
   const onSignupClick = () => {
     console.log("signup");
-    if (username && password) {
+    if (username && password && password === passwordConfirm) {
       signup(username, password);
       console.log();
     } else {
-      setError("Please enter a username and password");
+      if (password !== passwordConfirm) {
+        setError("Passwords do not match");
+      } else {
+        setError("Please enter a username and password");
+      }
     }
   };
 
@@ -26,6 +31,12 @@ export default function Signup() {
       <h1 className="title-text">Sign Up</h1>
       <TextInput title="Username" setState={setUsername} type="text" />
       <TextInput title="Password" setState={setPassword} type="password" />
+      <TextInput
+        title="Confirm password"
+        setState={setPasswordConfirm}
+        type="password"
+      />
+
       <button className="auth-button" onClick={() => onSignupClick()}>
         Sign Up
       </button>
@@ -33,7 +44,10 @@ export default function Signup() {
       {authError && <p className="text-red-500 my-4">{authError}</p>}
       {currentUser && <Navigate to="/"></Navigate>}
       <p>
-        Already have an account? <Link to="/login" className="text-blue-400">Log In</Link>
+        Already have an account?{" "}
+        <Link to="/login" className="text-blue-400">
+          Log In
+        </Link>
       </p>
     </div>
   );
