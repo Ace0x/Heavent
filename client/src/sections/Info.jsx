86d302f975f326c69b823d99340900c5948b50a2
Do Hyun Nam <dollis2002@gmail.com>
2022-06-14 10:33:37 -0500

    Added info

--- client/src/sections/Info.jsx
+++ client/src/sections/Info.jsx
@@ -2,15 +2,23 @@ import React from "react";
 
 export default function Info() {
   return (
-    <section
-      id="info"
-    >
+    <section id="info" >
       <h1 className="title-text m-10 text-4xl text-center">What's HeaVen't?</h1>
-      <p className="subtitle-text text-xl text-center">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ipsum,
-        tempore quidem nesciunt eos nostrum tempora magnam repellendus libero
-        blanditiis reprehenderit incidunt qui molestias fugiat velit! Dolor,
-        esse expedita. Tempore.
+      <p className="subtitle-text text-lg text-center">
+        Heaven't is a web game developed with the objective of being an
+        introduction to videogame design in mind. The user is able to design
+        their own levels and play levels designed by other players with the main
+        player, Godn't.
+        <br />
+        <br />
+        <em className="title-text text-6xl text-center m-10 p-10">
+          "Create them, play them, beat them"
+        </em>
+        <br />
+        <br />
+        In addition, using the website you'll be able to see the general stats
+        and user-specific stats for each level, and compare your stats against
+        other's playthroughs.
       </p>
     </section>
   );
