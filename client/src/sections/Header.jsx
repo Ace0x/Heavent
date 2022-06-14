0e57ba3ca8ba790867f39acc9b1c61b019af024a
Do Hyun Nam <dollis2002@gmail.com>
2022-06-14 13:35:38 -0500

    cc

--- client/src/sections/Header.jsx
+++ client/src/sections/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth } from "../context/authContext";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   const { currentUser } = useAuth();
