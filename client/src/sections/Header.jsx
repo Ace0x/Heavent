b53b7806c9ca33a33bbea682c5b7f24f7ff8862d
Do Hyun Nam <dollis2002@gmail.com>
2022-06-15 07:15:14 -0500

    Ortography

--- client/src/sections/Header.jsx
+++ client/src/sections/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
       <h1 className="title-text text-center text-8xl shadow-2xl">
         Hea<span style={{ fontSize: "12rem" }}>V</span>en't
       </h1>
-      <p className="subtitle-text">Heaven doens't lie here</p>
+      <p className="subtitle-text">Heaven doesn't lie here</p>
       <br />
       <Link to="/game">
         <div className="border-white border-4 hover:rounded-full rounded-lg bg-transparent subtitle-text px-8 text-4xl m-8">
